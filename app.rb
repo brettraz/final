783c8ffc140556a8b4f8116f1826244311b43f20
brettraz <brett.rasmussen@kellogg.northwestern.edu>
2020-03-04 11:06:26 +0000

    good through :id

--- app.rb
+++ app.rb
@@ -13,5 +13,36 @@ before { puts; puts "--------------- NEW REQUEST ---------------"; puts }       
 after { puts; }                                                                       #
 #######################################################################################
 
-events_table = DB.from(:events)
-rsvps_table = DB.from(:rsvps)
+items_table = DB.from(:items)
+customer_input_table = DB.from(:customer_input)
+
+get "/" do
+
+    view "home"
+end
+
+
+
+get "/items" do
+    puts "params: #{params}"
+
+    puts items_table.all
+    @items = items_table.all
+    view "items"
+end
+
+get "/items/:id" do
+    @item = items_table.where(id: params[:id]).first
+    view "item"
+
+    # pp items_table.where(id: params[:id]).to_a[0]
+    # @item = items_table.where(id: params[:id]).to_a[0]
+    # view "item"
+end
+
+get "/items/:id/customer_input/new" do
+    puts "params: #{params}"
+
+    
+    view "new_customer_input"
+end
--- createdb.rb
+++ createdb.rb
@@ -5,31 +5,141 @@ DB = Sequel.connect(connection_string)                                          
 #######################################################################################
 
 # Database schema - this should reflect your domain model
-DB.create_table! :events do
+DB.create_table! :items do
   primary_key :id
-  String :title
+  Boolean :status_available
+  String :item_type 
+  String :item_name 
+  String :condition 
   String :description, text: true
-  String :date
-  String :location
+  String :price
+  
 end
-DB.create_table! :rsvps do
+
+  
+ 
+
+DB.create_table! :customer_input do
   primary_key :id
-  foreign_key :event_id
-  Boolean :going
-  String :name
-  String :email
-  String :comments, text: true
+  foreign_key :item_id
+  String :customer_name
+  String :customer_phone_number
+  String :customer_e_mail
+  String :desired_pick_up_times, text: true
 end
-
 # Insert initial (seed) data
-events_table = DB.from(:events)
+items_table = DB.from(:items)
 
-events_table.insert(title: "Bacon Burger Taco Fest", 
-                    description: "Here we go again bacon burger taco fans, another Bacon Burger Taco Fest is here!",
-                    date: "June 21",
-                    location: "Kellogg Global Hub")
 
-events_table.insert(title: "Kaleapolooza", 
-                    description: "If you're into nutrition and vitamins and stuff, this is the event for you.",
-                    date: "July 4",
-                    location: "Nowhere")
+items_table.insert(item_name: "child gates", 
+                    description: "3 sets of gates to hook up in your home to protect your young kids from stairs and keep pets on one level.  Child proof to open and close.",
+                    item_type: "child gates",
+                    condition: "like new",
+                    price: "$30",
+                    status_available: 1)
+                    
+
+                    items_table.insert(item_name: "Child Crib and bed", 
+                    description: "3 in 1 crib, toddler bed, bed with gates.  This will accomodate your child's sleeping needs from infant to adolescent all in this convertible set.",
+                    item_type: "furniture",
+                    condition: "Used but stil good condition",
+                    price: "$85",
+                    status_available: 1)
+
+                    items_table.insert(item_name: "kids bike", 
+                    description: "Buzz lightyear bike with removable training wheels",
+                    item_type: "kids bike",
+                    condition: "Used",
+                    price: "$10",
+                    status_available: 1)
+
+                     items_table.insert(item_name: "kids balance bike", 
+                    description: "Balance bike, no pedals.  great for kids helping them to learn to ride.  Push off the ground like a skateboard.",
+                    item_type: "kids bike",
+                    condition: "Used",
+                    price: "$10",
+                    status_available: 1)
+
+                     items_table.insert(item_name: "Rogue Women's Barbell", 
+                    description: "35 lb olympic rogue barbell",
+                    item_type: "exercise equipment",
+                    condition: "like new",
+                    price: "$150",
+                    status_available: 1)
+
+                     items_table.insert(item_name: "kids bike", 
+                    description: "Buzz lightyear bike with removable training wheels",
+                    item_type: "kids bike",
+                    condition: "Used",
+                    price: "$10",
+                    status_available: 1)
+
+                     items_table.insert(item_name: "Child Crib and bed", 
+                    description: "3 in 1 crib, toddler bed, bed with gates.  This will accomodate your child's sleeping needs from infant to adolescent all in this convertible set.",
+                    item_type: "furniture",
+                    condition: "Used but stil good condition",
+                    price: "$85",
+                    status_available: 1)
+
+                     items_table.insert(item_name: "Rogue Climbing Rope", 
+                    description: "Rogue 15 foot climbing rope.  Great for gymnastics and crossfit.",
+                    item_type: "exercise equipment",
+                    condition: "Used but stil good condition",
+                    price: "$50",
+                    status_available: 1)
+
+                     items_table.insert(item_name: "Rogue gymnastics hanging rings", 
+                    description: "Rogue wooden gymnastics rings.  Great for gymnastics and crossfit.  Use them for dips, muscle ups, push-ups, etc.",
+                    item_type: "exercise equipment",
+                    condition: "Used but stil good condition",
+                    price: "$50",
+                    status_available: 1)
+
+                     items_table.insert(item_name: "Child dresser", 
+                    description: "5 drawer dark wood dresser, still in great condition.  Perfect for any kid.",
+                    item_type: "furniture",
+                    condition: "Used but stil good condition",
+                    price: "$50",
+                    status_available: 1)
+                    
+                     items_table.insert(item_name: "Body Solid GHD machine",
+                    description: "The best machine on the market for core, ham, butt.  Get chiseled.",
+                    item_type: "exercise equipment",
+                    condition: "Used but stil good condition",
+                    price: "$250",
+                    status_available: 1)
+
+                     items_table.insert(item_name: "Original WWF wrestling buddies", 
+                    description: "Macho King Randy Savage in all his glory.  Collectors item.  Just like you remember from your youth.",
+                    item_type: "toy",
+                    condition: "Used but stil great condition",
+                    price: "$15",
+                    status_available: 1)
+
+                     items_table.insert(item_name: "Original WWF wrestling buddies", 
+                    description: "The Ultimate Warrior in all his glory.  Collectors item.  Just like you remember from your youth.",
+                    item_type: "toy",
+                    condition: "Used but stil great condition",
+                    price: "$15",
+                    status_available: 1)
+
+                     items_table.insert(item_name: "Original WWF wrestling buddies", 
+                    description: "Hulk Hogan in all his glory.  Collectors item.  Just like you remember from your youth.",
+                    item_type: "toy",
+                    condition: "Used but stil great condition",
+                    price: "$15",
+                    status_available: 1)
+
+                     items_table.insert(item_name: "Original WWF wrestling buddies", 
+                    description: "The Million Dollar Man Ted Dibiase in all his glory.  Everybody's got a price.  Collectors item.  Just like you remember from your youth.",
+                    item_type: "toy",
+                    condition: "Used but stil great condition",
+                    price: "$15",
+                    status_available: 1)
+
+                     items_table.insert(item_name: "Thomas the Train", 
+                    description: "Enormous collection of Thomas trains, island, toys.  You can come see before purchasing.",
+                    item_type: "toy",
+                    condition: "Used",
+                    price: "$100",
+                    status_available: 1)
